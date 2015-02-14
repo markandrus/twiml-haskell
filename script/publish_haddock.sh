fcb4b2b393ad7ada59dea1b955c2dd12eb37de89
Mark Roberts <markandrusroberts@gmail.com>
2015-02-14 15:25:03 -0800

    Add publish_haddock.sh step

---
+++ script/publish_haddock.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Description: This script is run by Travis CI on successful builds in order to
+# update the repo's GitHub pages with Haddock documentation. 
+set -e
+
+user='markandrus'
+repo='twiml-haskell'
+root='src/Text/XML/Twiml.hs'
+
+if [[ "${TRAVIS_REPO_SLUG}"  == "${user}/${repo}" \
+   && "${TRAVIS_PULL_REQUEST}" == 'false' \
+   && "${TRAVIS_BRANCH}" == 'master' ]]
+then
+
+  echo "Generating Haddock documentation..."
+  cabal install haddock --global
+  haddock -o doc --html ${root}
+  cp -R doc ${HOME}/doc
+
+  echo "Cloning repo..."
+  cd ${HOME}
+  git config --global user.email 'travis@travis-ci.org'
+  git config --global user.name 'travis-ci'
+  git clone --quiet --branch=gh-pages ${repo} gh-pages >/dev/null
+  
+  echo "Updating repo..."
+  cd gh-pages
+  rm -rf *
+  mv ../doc/* gh-pages
+  git add -f .
+  git commit -m "Updating Haddock documentation (${TRAVIS_BUILD_NUMBER})"
+  git push -fq origin gh-pages >/dev/null
+  
+  echo "Published Haddock documentation."
+
+fi

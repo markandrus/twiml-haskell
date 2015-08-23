1868adf1f204b345af8de853c7400d66c8ac8b8d
Mark Andrus Roberts <markandrusroberts@gmail.com>
2015-08-22 18:22:18 -0700

    Revert to Simple build-type

--- Setup.hs
+++ Setup.hs
@@ -1,10 +1,2 @@
 import Distribution.Simple
-import System.Process
-
-main
-  = defaultMainWithHooks
-  $ simpleUserHooks {
-      preBuild = \_ _ -> do
-        system "make"
-        return (Nothing, [])
-    }
+main = defaultMain
